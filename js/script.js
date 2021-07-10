f9c3d1478ff52421ed1a14d4b212e582eef76546
je-joue <maria.v.bushkova@yandex.ru>
2021-07-11 02:23:45 +0300

    fin

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 //Карточки по умолчанию
-// const initialCards = [
-//   {
-//     text: 'Камчатка';
-//     link: 'https://images.unsplash.com/photo-1535557142533-b5e1cc6e2a5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80';
-//   },
-//   {
-//     text: 'Красноярский край';
-//     link: 'https://images.unsplash.com/photo-1597125760773-b0166e249ea7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80';
-//   },
-//   {
-//     text: 'Сочи';
-//     link: 'https://images.unsplash.com/photo-1604953364318-dcf6d8273c0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80';
-//   },
-//   {
-//     text: 'Москва';
-//     link: 'https://images.unsplash.com/photo-1612966809470-6967deb36552?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80';
-//   },
-//   {
-//     text: 'Хребет Ицыл';
-//     link: 'https://images.unsplash.com/photo-1517229349791-2af54b4cb18e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1056&q=80';
-//   },
-//   {
-//     text: 'Озеро Ольхон';
-//     link: 'https://images.unsplash.com/photo-1501675423372-9bfa95849e62?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80';
-//   }
-// ];
+const initialCards = [
+  {
+    text: 'Камчатка',
+    link: 'https://images.unsplash.com/photo-1535557142533-b5e1cc6e2a5d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1101&q=80'
+  },
+  {
+    text: 'Красноярский край',
+    link: 'https://images.unsplash.com/photo-1597125760773-b0166e249ea7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80'
+  },
+  {
+    text: 'Сочи',
+    link: 'https://images.unsplash.com/photo-1604953364318-dcf6d8273c0d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'
+  },
+  {
+    text: 'Москва',
+    link: 'https://images.unsplash.com/photo-1612966809470-6967deb36552?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80'
+  },
+  {
+    text: 'Хребет Ицыл',
+    link: 'https://images.unsplash.com/photo-1517229349791-2af54b4cb18e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1056&q=80'
+  },
+  {
+    text: 'Озеро Ольхон',
+    link: 'https://images.unsplash.com/photo-1501675423372-9bfa95849e62?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  }
+];
 
 const profileEditPopup = document.querySelector('#profile-edit-popup');
 const profileEditButton = document.querySelector('.profile__edit-button');
@@ -39,19 +39,18 @@ const editActivity = document.querySelector('#edit-activity');
 const profileEditSubmitButton = profileEditPopup.querySelector('.form__submit-button');
 const addCardSubmitButton = addCardPopup.querySelector('.form__submit-button');
 const addCardForm = document.querySelector('#add-card-form');
-
 const photoContent = document.querySelector('.photo-content__cards');
 const cardName = document.querySelector('#card-name');
 const cardLink = document.querySelector('#card-link');
-
 const imagePopup = document.querySelector('#image-popup');
+const imagePopupCloseButton = document.querySelector('#image-popup-close');
 
-// открытие popup
+// открытие pop-up
 function popupOpen(popup) {
   popup.classList.add('popup_open');
 }
 
-// закрытие popup
+// закрытие pop-up
 function popupClose(popup) {
   popup.classList.remove('popup_open');
 }
@@ -87,6 +86,14 @@ addCardCloseButton.addEventListener('click', function() {
   popupClose(addCardPopup);
 });
 
+// открытие pop-up с картинкой
+
+
+// закрытие pop-up с картинкой
+imagePopupCloseButton.addEventListener('click', function() {
+  popupClose(imagePopup);
+});
+
 // Создание новой карточки
 function addCard(item) {
   const cardTemplate = document.querySelector('#card-template').content;
@@ -103,7 +110,11 @@ function addCard(item) {
     newCard.querySelector('.card__like').classList.toggle('card__like_active');
   });
 
-  newCard.querySelector('.card__photo').addEventListener('click', function() {
+  newCard.querySelector('.card__photo').addEventListener('click', function(item) {
+    const popupImage = imagePopup.querySelector('.popup__image');
+    const popupImageCaption = imagePopup.querySelector('.popup__image-caption');
+    popupImage.src = newCard.querySelector('.card__photo').getAttribute('src');
+    popupImageCaption.textContent = newCard.querySelector('.card__text').textContent;
     popupOpen(imagePopup);
   });
 
@@ -122,3 +133,7 @@ addCardSubmitButton.addEventListener('click', function(event) {
   addCardForm.reset();
 });
 
+// 6 карточек "из коробки"
+initialCards.forEach (function(item) {
+  photoContent.append(addCard(item));
+});
