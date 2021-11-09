b7875fbd6076528c266ec7d3b2c3c23c979c650d
je-joue <maria.v.bushkova@yandex.ru>
2021-11-09 20:58:34 +0300

    структура файлов

---
+++ components/modal.js
@@ -0,0 +1,9 @@
+// открытие pop-up
+function popupOpen(popup) {
+  popup.classList.add('popup_open');
+}
+
+// закрытие pop-up
+function popupClose(popup) {
+  popup.classList.remove('popup_open');
+}
